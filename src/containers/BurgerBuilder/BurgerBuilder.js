16c848c158e8da84b67a2b29bd98866d3e6ce96d
Shivendra <shivendra1308@gmail.com>
2019-06-23 15:02:27 +0530

    Initial Setup

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
+        return (
+            <Auxiliary>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
