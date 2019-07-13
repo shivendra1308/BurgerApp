6e9d4c8d26fc3cf317cd348008e69a097a26d6ae
Shivendra <shivendra1308@gmail.com>
2019-07-13 16:18:09 +0530

    My method applied

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,24 +4,35 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
-    let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
-            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />;
-            } );
-        } )
-        .reduce((arr, el) => {
-            return arr.concat(el)
-        }, []);
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>;
+
+    // This is my method
+    let trans = [] ;
+     for(let keys in props.ingredients){
+        let   value = [props.ingredients[keys]];
+         for( let i = 0; i <= value; i++ ) {
+           trans.push( <BurgerIngredient key={keys + i} type={keys} />) ;   
+         };
     }
+
+    // let transformedIngredients = Object.keys( props.ingredients )
+    //     .map( igKey => {
+    //         return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+    //             return <BurgerIngredient key={igKey + i} type={igKey} />;
+    //         } );
+    //     } )
+    //     .reduce((arr, el) => {           
+    //         return arr.concat(el)
+    //     }, []);
+    // if (transformedIngredients.length === 0) {
+    //     transformedIngredients = <p>Please start adding ingredients!</p>;
+    // }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            {transformedIngredients}
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="salad" />
+            {/* {transformedIngredients}   This method was described by Tutor  */}
+            {trans}  
+            {/* This is my method */}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
